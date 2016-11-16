ddb05d8be665d4de4a071782c5494cb0536ff47f
Kevin Merckx <kevin@interfacewerk.de>
2016-11-16 16:29:03 +0100

    feat(home): display modal on ctrl+e

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  shouldDisplayModal: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  displayModal($event: KeyboardEvent) {
+    if ($event.key === 'e' && $event.ctrlKey) {
+      this.shouldDisplayModal = true;
+    }
   }
 
 }
